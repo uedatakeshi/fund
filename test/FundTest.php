b940e6dfa34400e354c11ec86cdd5f9647d2e235
Takeshi Ueda <uedatakeshi@gmail.com>
2021-04-19 15:31:19 +0900

    add test of from to date

--- test/FundTest.php
+++ test/FundTest.php
@@ -20,9 +20,39 @@ class FundTest extends TestCase
     // テスト処理
     public function testGetName()
     {
-        $name = "yamada";
-
-        // 配列（$array）の要素数が３つであることをテストする。
+        $name = "ueda";
         $this->assertEquals($name, $this->fund->getName());
+    }
+
+    public function test_setFromTo()
+    {
+        $ms_code = "2018083102";
+        $from = "";
+        $to_date_time = mktime(0, 0, 0, date("m"), date("d") - 1, date("Y"));
+        $data = [
+            'selectStdYearFrom' => "2018",
+            'selectStdMonthFrom' => "08",
+            'selectStdDayFrom' => "31",
+            'selectStdYearTo' => date("Y", $to_date_time),
+            'selectStdMonthTo' => date("m", $to_date_time),
+            'selectStdDayTo' => date("d", $to_date_time),
+        ];
+        $this->assertEquals($data, $this->fund->_setFromTo($ms_code, $from));
+    }
+
+    public function test_setFromTo_fromari()
+    {
+        $ms_code = "2018083102";
+        $from = "2021-12-31";
+        $to_date_time = mktime(0, 0, 0, date("m"), date("d") - 1, date("Y"));
+        $data = [
+            'selectStdYearFrom' => "2021",
+            'selectStdMonthFrom' => "12",
+            'selectStdDayFrom' => "31",
+            'selectStdYearTo' => date("Y", $to_date_time),
+            'selectStdMonthTo' => date("m", $to_date_time),
+            'selectStdDayTo' => date("d", $to_date_time),
+        ];
+        $this->assertEquals($data, $this->fund->_setFromTo($ms_code, $from));
     }
 }
